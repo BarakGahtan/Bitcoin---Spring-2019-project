f833cd38712f608624a6dcf61858dbc721ed1ca3
LIORD <liordavid14@technion.campus.ac.il>
2019-05-17 17:54:18 +0300

    hi3

--- our_maker.py
+++ our_maker.py
@@ -1,8 +1,15 @@
 import bitcoin_core_connection_setup
+import key_generator
 import connection
+import ecdsa
 
-connection = bitcoin_core_connection_setup.connect_to_node()
-connection.createrawtransaction()
+man = key_generator.Person()
+man.key_generator_func()
+
+connection1 = bitcoin_core_connection_setup.connect_to_node()
+connection1.createrawtransaction([{"txid": "a9d4599e15b53f3eb531608ddb31f48c695c3d0b3538a6bda871e8b34f2f430c",
+                  "vout": 0}],
+                {"mkZBYBiq6DNoQEKakpMJegyDbw2YiNQnHT":50})
 
 
 
