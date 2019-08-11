de3fa3a6751cdedcac44a78ee81c48682599430f
gahta <barakgahtan@gmail.com>
2019-08-11 10:55:28 +0300

    aaa

--- our_maker.py
+++ our_maker.py
@@ -7,10 +7,10 @@ man = key_generator.Person()
 man.key_generator_func()
 
 connection1 = bitcoin_core_connection_setup.connect_to_node()
+
 connection1.createrawtransaction([{"txid": "a9d4599e15b53f3eb531608ddb31f48c695c3d0b3538a6bda871e8b34f2f430c",
                   "vout": 0}],
                 {"mkZBYBiq6DNoQEKakpMJegyDbw2YiNQnHT":50})
 
-
-
+print(connection1.setgenerate(101))
 
