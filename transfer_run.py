6e7e423c6f81db0c3d9e436dc6ca9ffe2bec0dfd
gahta <barakgahtan@gmail.com>
2019-08-26 14:35:02 +0300

    new1632

--- transfer_run.py
+++ transfer_run.py
@@ -16,7 +16,7 @@ def process_exists(process_name):
     # because Fail message could be translated
     return last_line.lower().startswith(process_name.lower())
 
-def send_to_address(source_address,source_private_key,block_id,destination,amount):
+def send_to_address(source_address,privateArray,block_id,destination,amount):
     block_struct = connection1.getblock(block_id,2)
     txid_struct = block_struct.get("tx")
     txid_number = txid_struct[0].get("txid")
@@ -25,29 +25,36 @@ def send_to_address(source_address,source_private_key,block_id,destination,amoun
     scriptPubKey = txid_scriptPubKey.get("hex")
     hash_to_be_sent = connection1.createrawtransaction([{"txid": txid_number, "vout": 0}], {destination:25,source_address:24.95})
     param_input = [{"txid": txid_number, "vout": 1,"scriptPubKey": scriptPubKey}]
-    private_keys =[]
-    private_keys.append(source_private_key)
-    signed = connection1.signrawtransactionwithkey(hash_to_be_sent, private_keys,param_input)
-    print(signed)
+    signed = connection1.signrawtransactionwithkey(hash_to_be_sent, privateArray,param_input)
     hex_hash_sign = signed.get("hex")
     result = connection1.sendrawtransaction(hex_hash_sign)
     return result
 
+def send_from_multisig(source_address,privateArray,block_id,destination,amount):
+    block_struct = connection1.getblock(block_id,2)
+    txid_struct = block_struct.get("tx")
+    txid_number = txid_struct[0].get("txid")
+    vout_struct = txid_struct[0].get("vout")
+    txid_scriptPubKey = vout_struct[0].get("scriptPubKey")
+    scriptPubKey = txid_scriptPubKey.get("hex")
+    hash_to_be_sent = connection1.createrawtransaction([{"txid": txid_number, "vout": 0}], {destination:25,source_address:24.95})
+    param_input = [{"txid": txid_number, "vout": 1,"scriptPubKey": scriptPubKey, "redeemScript": redeem_script }]
+    signed = connection1.signrawtransactionwithkey(hash_to_be_sent, privateArray,param_input)
+    hex_hash_sign = signed.get("hex")
+    result = connection1.sendrawtransaction(hex_hash_sign)
+    return result
 
+##intialize system
 if process_exists('bitcoind.exe'):
     os.system('TASKKILL /F /IM bitcoind.exe')
 time.sleep(3)
-
-myfolder1 = "C:\\Users\\gahta\\AppData\\Roaming\\Bitcoin\\regtest"
-if os.path.exists(myfolder1):
-    shutil.rmtree(myfolder1)
+myPath = "C:\\Users\\gahta\\AppData\\Roaming\\Bitcoin\\regtest"
+if os.path.exists(myPath):
+    shutil.rmtree(myPath)
 subprocess.Popen(["C:\\Program Files\\Bitcoin\\daemon\\bitcoind.exe", "-regtest","-deprecatedrpc=generate"])
 time.sleep(7)
 
-
-
-
-
+#generate key for players
 A = key_generator.Person()
 A.key_generator_func()
 B = key_generator.Person()
@@ -74,32 +81,9 @@ ADPrivateKeys.append(A.private_key)
 ADPrivateKeys.append(D.private_key)
 redeem_script_A = []
 redeem_script_A.append(structA[0])
-
-#send_A_to_A_D = send_to_address(A.address,A.private_key,id_of_block_A,A_D_address,25)
-#send_D_to_A_D = send_to_address(D.address,D.private_key,id_of_block_D,A_D_address,25)
-send_A_to_B  =  send_to_address(A.address,A.private_key,idBlockA,B.address,25)
-
-structAD = connection1.addmultisigaddress(2,ADPrivateKeys)
-addressAD = structAD.get("address")
-
-redeem_script = structAD.get("redeemScript")
-
-generate_struct = connection1.generatetoaddress(101,addressAD)
-id_of_block = generate_struct[0]
-print("total balance:",connection1.getbalance())
-
-block_struct = connection1.getblock(id_of_block,2)
-txid_struct = block_struct.get("tx")
-txid_number = txid_struct[0].get("txid")
-vout_struct = txid_struct[0].get("vout")
-txid_scriptPubKey = vout_struct[0].get("scriptPubKey")
-scriptPubKey = txid_scriptPubKey.get("hex")
-
-hash_to_be_sent = connection1.createrawtransaction([{"txid": txid_number, "vout": 0}], {itay.address:49.95})
-param_input = [{"txid": txid_number, "vout": 1,"scriptPubKey": scriptPubKey, "redeemScript": redeem_script }]
-signed = connection1.signrawtransactionwithkey(hash_to_be_sent,ADPrivateKeys,param_input)
-print(signed)
-signed_hex = signed.get("hex")
-send = connection1.sendrawtransaction(signed_hex)
-print(send)
-print("DONE")
+AprivateArray = []
+AprivateArray.append(A.private_key)
+info1 = connection1.getmininginfo()
+send_A_to_B  =  send_to_address(A.address,AprivateArray,idBlockA,B.address,25)
+info2 = connection1.getmininginfo()
+print("sign")
